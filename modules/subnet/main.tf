6fe4c1be29400c26ec6cda40e373bc22a148faed
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-18 13:28:12 +0530

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public" {
   count = 2
-  vpc_id = var.vpc_id
+  vpc_id = module.vpc.vpc_id
   cidr_block = cidrsubnet("10.0.0.0/16", 4, count.index)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
