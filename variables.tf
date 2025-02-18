27b2b213d7efe8b2aae0a5732bae075b85615652
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-18 11:34:22 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instances"
+}
+
+variable "web_instance_type" {
+  description = "EC2 instance type for the web servers"
+  default     = "t2.micro"
+}
+
+variable "db_instance_class" {
+  description = "RDS instance type for the database"
+  default     = "db.t3.micro"
+}
+
+variable "db_name" {
+  description = "Database name"
+}
+
+variable "db_username" {
+  description = "Database username"
+}
+
+variable "db_password" {
+  description = "Database password"
+}
