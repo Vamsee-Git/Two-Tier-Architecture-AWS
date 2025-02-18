bdfc497aab1db9cabd44f5bcb616b936f145012b
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-18 11:24:34 +0530

    Create main.tf

---
+++ modules/security_groups/main.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "web" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db" {
+  name        = "db-sg"
+  description = "Allow MySQL traffic"
+  
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
+
+output "web_sg_id" {
+  value = aws_security_group.web.id
+}
+
+output "db_sg_id" {
+  value = aws_security_group.db.id
+}
