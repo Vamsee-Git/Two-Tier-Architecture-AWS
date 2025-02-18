d9f4d9b1c95a85d4c90ab3c8e1792a483713652e
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-18 07:41:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,29 +2,12 @@ provider "aws" {
   region = "ap-south-1"  # Update to your preferred AWS region
 }
 
-# Create DynamoDB Table for State Locking
-resource "aws_dynamodb_table" "terraform_lock_table" {
-  name         = "terraform-lock-table"
-  hash_key     = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "TerraformStateLockTable"
-  }
-}
-
 # S3 Backend Configuration for Remote State Management
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-two-tier-vamsee"  # Replace with your S3 bucket name
     key            = "state.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = aws_dynamodb_table.terraform_lock_table.name  # Reference the DynamoDB table
     encrypt        = true
   }
 }
