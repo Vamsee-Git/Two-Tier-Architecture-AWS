a80d86a19ada0d9c77294a3d1a6f312133ca1a33
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-21 15:41:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"  # Replace with your desired region.
 }
 
-# VPC
+# VPC creation
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
