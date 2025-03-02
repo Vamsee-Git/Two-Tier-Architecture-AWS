2122da906e9a32f18fd132b6c929945a7623ac19
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-02 20:46:57 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "web_server_1_public_ip" {
 output "web_server_2_public_ip" {
   value = aws_instance.web_server_2.public_ip
 }
-# Outputs for RDS Instance Endpoint.
+# Outputs for RDS Instance Endpoint
 output "rds_endpoint" {
   value = aws_db_instance.mysql_db.endpoint
 }
